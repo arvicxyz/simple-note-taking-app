bcd98b2ae1dd3ddd121501612eb7262b86f4f660
Amir Fahd A. Hadji Usop <dudeamirfahd@gmail.com>
2024-01-09 17:44:02 +0800

    Move project to outer folder

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: false,
       ),
       home: Container(
-        color: Colors.red,
+        color: Colors.blue,
       ),
     );
   }
