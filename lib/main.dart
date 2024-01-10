40fae70671181247192c0016fcb34ee6ecb7f7d3
Amir Fahd A. Hadji Usop <dudeamirfahd@gmail.com>
2024-01-10 11:04:55 +0800

    Hide debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class NoteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Note App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
